0993a759da19928e6b806b7dd3cfde55e0f73b41
Brooke Wu <brookewu2023@gmail.com>
2026-03-17 13:46:11 -0400

    Implement basic calling external Web API from client component

--- BlazorApp/BlazorApp/Program.cs
+++ BlazorApp/BlazorApp/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Provide an HTTPClient to the app so that server-based components can call external web APIs using HTTPClient instances (Source: https://learn.microsoft.com/en-us/aspnet/core/blazor/call-web-api?view=aspnetcore-10.0#server-side-scenarios-for-calling-external-web-apis)
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
